a302b51af49ef70e31f5103f78d9c0d5eaa56c39
Michael Ferraro <mferraro2304@gmail.com>
2021-10-13 10:40:31 -0400

    more organization

--- Java/CSCI/Week6/class6.java
+++ Java/CSCI/Week6/class6.java
@@ -3,6 +3,20 @@ import java.util.Scanner;
 
 public class class6 {
     public static void main(String[] args) {
-        
+      Scanner scnr = new Scanner(System.in);
+      int guess, answer;
+      guess = 0;
+      answer = 5;
+      
+      int guesses = 0;
+
+      while(guess != answer){
+          System.out.println(("Enter your guess: "));
+          guess = scnr.nextInt();
+          guesses = guesses + 1;
+      }
+      System.out.println("It took you " + guesses + " guesses to guess the answer.\n");
+
+      scnr.close();
     }
 }
