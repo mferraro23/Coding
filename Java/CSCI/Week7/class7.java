d6a6dfc3eb9d0e3b5e9d021f57159c7b522ab0be
Michael Ferraro <mferraro2304@gmail.com>
2021-10-18 11:34:37 -0400

    lab work today

---
+++ Java/CSCI/Week7/class7.java
@@ -0,0 +1,22 @@
+package Week7;
+import java.util.Scanner;
+
+public class class7 {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        String msg = scnr.nextLine();
+        int stringLength = msg.length();
+        String newString = "";
+        while(!msg.equals("done") && !msg.equals("d") && !msg.equals("Done")){
+            for (int i = stringLength-1; i >= 0; i-- ){
+                newString = newString + msg.charAt(i);
+            }
+            System.out.println(newString);
+            msg = scnr.nextLine();
+            stringLength = msg.length();
+            newString = "";
+        }
+        
+        scnr.close();
+    }
+}       
